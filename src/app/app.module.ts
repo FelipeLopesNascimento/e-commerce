f06eb9cb543e344c5b837bf3033672a188e0bfd0
Felipe Lopes do Nascimento <felipelopesnascimento@hotmail.com>
2024-10-08 16:37:57 -0300

    Footer finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,16 @@ import { SearchComponent } from './components/header/search/search.component';
 import { IconsComponent } from './components/header/icons/icons.component';
 import { HeaderComponent } from './components/header/header.component';
 import { CategoriasComponent } from './components/header/categorias/categorias.component';
+import { PoliciesComponent } from './components/footer/policies/policies.component';
+import { HelpComponent } from './components/footer/help/help.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AboutusComponent } from './components/footer/aboutus/aboutus.component';
+import { ContactComponent } from './components/footer/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ContentComponent } from './components/content/content.component';
+import { ProductComponent } from './components/content/product/product.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+
 
 
 @NgModule({
@@ -18,13 +28,24 @@ import { CategoriasComponent } from './components/header/categorias/categorias.c
     SearchComponent,
     IconsComponent,
     HeaderComponent,
-    CategoriasComponent
+    CategoriasComponent,
+    PoliciesComponent,
+    HelpComponent,
+    FooterComponent,
+    AboutusComponent,
+    ContactComponent,
+    HomeComponent,
+    ContentComponent,
+    ProductComponent,
+    SidebarComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    NgbModule
+    NgbModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
