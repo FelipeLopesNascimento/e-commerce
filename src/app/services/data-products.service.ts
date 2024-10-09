bad27d0465a250a35730253123d942737ca7d61a
Felipe Lopes do Nascimento <felipelopesnascimento@hotmail.com>
2024-10-08 21:03:14 -0300

    Banco de dados conectado

--- src/app/services/data-products.service.ts
+++ src/app/services/data-products.service.ts
@@ -1,17 +1,18 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataProductsService {
 
-  private jsonUrl = 'data/dataProducts.json';
+  private apiUrl = 'http://localhost:3000/dataProducts';
 
   constructor(private http: HttpClient) { }
 
   getProducts(): Observable<any> {
-    return this.http.get<any>(this.jsonUrl);
+    return this.http.get<any>(this.apiUrl);
   }
 }
